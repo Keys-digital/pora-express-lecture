9b07a2522905d7af40a8d6fb569a2af52df2ca7b
felixfavour <felixfavour0@gmail.com>
2024-11-07 16:07:52 +0100

    chore: successfully created our first endpoint

--- index.js
+++ index.js
@@ -3,6 +3,22 @@ const app = express()
 
 const PORT = 5200
 
+// MAIN ROUTE
+app.get("/", function (request, response) {
+  response.send("Welcome to my express server")
+})
+
+// ENDPOINTS AND ROUTES
+app.get("/api", function (request, response) {
+  response.status(200).json({
+    error: false,
+    message: "Welcome to the API section of my express server",
+  })
+})
+
+// JSON (is synonymous to) Objects in Javascript
+// JSON = JavaScript Object Notation
+
 app.listen(PORT, function () {
   console.log(`App server has started on http://localhost:${PORT}`)
 })
